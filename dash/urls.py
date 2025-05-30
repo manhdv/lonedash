398f79bd44c5b295546dbbe6c3d5f307f292bda7
Doan Manh <doanvanmanh87@gmail.com>
2025-05-30 10:32:46 +0700

    Remove unused files

--- dash/urls.py
+++ dash/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import dash_view, pages_view, posts_view, users_view, login_view, edit_view
+from .views import dash_view
 
 urlpatterns = [
     path('dash/', dash_view, name='dash'),
-    path('pages/', pages_view, name='pages'),
-    path('posts/', posts_view, name='posts'),
-    path('users/', users_view, name='users'),
-    path('login/', login_view, name='login'),
-    path('edit/', edit_view, name='edit'),
 ]
--- dash/views.py
+++ dash/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
 from dash.models import EconomicData, Country
 from django.db.models import Max
-
+import os
 
 # Create your views here.
 def dash_view(request):
-    vietnam = Country.objects.get(name="Vietnam")
-    latest_dates = EconomicData.objects.filter(country=vietnam).values('indicator').annotate(latest=Max('date'))
-
-    data = []
-    for row in latest_dates:
-        ed = EconomicData.objects.get(country=vietnam, indicator=row['indicator'], date=row['latest'])
-        data.append(ed)
-    return render(request, 'index.html', {'indicators': data, 'page_title': 'Dashboard',
-        'page_subtitle': 'Manage Your Site',})
-
-
-def pages_view(request):
-    return render(request, 'pages.html')
-
-def posts_view(request):
-    return render(request, 'posts.html')
-
-def users_view(request):
-    return render(request, 'users.html')
-
-def login_view(request):
-    return render(request, 'login.html')
-
-def edit_view(request):
-    return render(request, 'edit.html')
+    current_dir = os.path.dirname(__file__)
+    svg_path = os.path.join(current_dir, 'static', 'icons', 'icons.svg')
+    with open(svg_path, 'r', encoding='utf-8') as f:
+        svg_content = f.read()
+    return render(request, 'dash.html', {'icons_svg': svg_content})
